40ee5e24f3ac3c0a9e144b9b6696be5e39c670ad
ktnyt <t11080hi@sfc.keio.ac.jp>
2014-11-25 06:45:09 +0900

    Release version

--- LeyeApp/LACollectionViewController.m
+++ LeyeApp/LACollectionViewController.m
@@ -1,10 +1,25 @@
-//
-//  LACollectionViewController.m
-//  LeyeApp
-//
-//  Created by kotone on 2014/04/19.
-//  Copyright (c) 2014年 Kotone Itaya. All rights reserved.
-//
+/******************************************************************************
+**  LACollectionViewController.m
+**  LeyeApp
+**
+**  Created by kotone on 2014/04/19.
+**  Copyright (c) 2014 Kotone Itaya. All rights reserved.
+**
+** This file is part of LeyeApp.
+**
+** LeyeApp is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** LeyeApp is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with LeyeApp.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
 
 #import "LACollectionViewController.h"
 
